2a9f3daf8ebf5e0fab07eed7754da7d3625f4874
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-06-20 08:42:53 -0700

    exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
-
 def safe_print_integer(value):
+
+
     try:
-        print("{:d}".format(value))
-        return True
-    except:
+        print('{:d}'.format(value))
+    except Exception as error:
         return False
+    else:
+        return True
