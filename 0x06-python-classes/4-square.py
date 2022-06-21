d97822231078f410c43abacba0be22e93549e496
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-06-21 08:16:15 -0700

    classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,35 +1,29 @@
-  
 #!/usr/bin/python3
-""" creates a square with size"""
+"""create an empty class called Square"""
 
 
 class Square:
-    """ create class
-    Attributes:
-        size: size of square
+    """create a square.
+    size (no type/value verification)
+    atributes:
+    size: private instance.
     """
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
         self.__size = size
 
     def area(self):
-        """ return area """
-        A = self.__size * self.__size
-        return (A)
+        return self.__size * self.__size
 
     @property
     def size(self):
-        """ return def """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
