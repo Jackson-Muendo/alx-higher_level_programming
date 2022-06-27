a7f29c7928959ea394be302a8c206cdf8d780f64
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-06-27 05:47:46 -0700

    classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
-'''
-Docstring goes here
-'''
-
-from sys import exit as ex
+"""Computes all possible solutions to the N-queens puzzle"""
 
 
-def some_func
+import sys
+
+def n_queen(i):
+    """prints a chess board"""
+    chess = [['' for c in range(i)] for r in range(i)]
+    print(chess)
