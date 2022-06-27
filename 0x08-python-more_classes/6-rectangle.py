e3b2817c18c33de984f67c1adbeaaeca974a5ed9
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-06-27 05:30:02 -0700

    classes

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,167 +1,75 @@
 #!/usr/bin/python3
-
-# 6-rectangle.py
-
-# Zakari Usman <zakariusman4real@gmail.com>
-
-"""Defines a Rectangle class."""
-
-
-
+"""Defines a class"""
 
 
 class Rectangle:
-
-    """Represent a rectangle.
-
-    Attributes:
-
-        number_of_instances (int): The number of Rectangle instances.
-
-    """
-
-
-
+    """Defines private instance attribute"""
     number_of_instances = 0
 
-
-
     def __init__(self, width=0, height=0):
-
-        """Initialize a new Rectangle.
-
-        Args:
-
-            width (int): The width of the new rectangle.
-
-            height (int): The height of the new rectangle.
-
-        """
-
+        """Instantiates the class"""
         type(self).number_of_instances += 1
-
-        self.width = width
-
-        self.height = height
-
-
+        self.__width = width
+        self.__height = height
 
     @property
-
     def width(self):
-
-        """Get/set the width of the Rectangle."""
-
+        """Retrieves the width of a rectangle"""
         return self.__width
 
-
-
     @width.setter
-
     def width(self, value):
-
-        if not isinstance(value, int):
-
+        """Sets the width of a rectangle"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-
         if value < 0:
-
             raise ValueError("width must be >= 0")
-
         self.__width = value
 
-
-
     @property
-
     def height(self):
-
-        """Get/set the height of the Rectangle."""
-
+        """Retrieves the height of the rectangle"""
         return self.__height
 
-
-
     @height.setter
-
     def height(self, value):
-
-        if not isinstance(value, int):
-
+        """Sets the height of the rectangle"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-
         if value < 0:
-
             raise ValueError("height must be >= 0")
-
         self.__height = value
 
-
-
     def area(self):
-
-        """Return the area of the Rectangle."""
-
-        return (self.__width * self.__height)
-
-
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
 
     def perimeter(self):
-
-        """Return the perimeter of the Rectangle."""
-
-        if self.__width == 0 or self.__height == 0:
-
-            return (0)
-
-        return ((self.__width * 2) + (self.__height * 2))
-
-
+        """Returns the perimeter of the rectangle"""
+        if self.__width == 0 and self.__height == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
 
     def __str__(self):
-
-        """Return the printable representation of the Rectangle.
-
-        Represents the rectangle with the # character.
-
-        """
-
-        if self.__width == 0 or self.__height == 0:
-
-            return ("")
-
-
-
-        rect = []
-
+        """Prints the rectangle"""
+        if self.__width == 0 and self.__height == 0:
+            return ""
+        new = []
         for i in range(self.__height):
-
-            [rect.append('#') for j in range(self.__width)]
-
+            for j in range(self.__width):
+                new.append('#')
             if i != self.__height - 1:
-
-                rect.append("\n")
-
-        return ("".join(rect))
-
-
+                new.append("\n")
+        return "".join(new)
 
     def __repr__(self):
-
-        """Return the string representation of the Rectangle."""
-
-        rect = "Rectangle(" + str(self.__width)
-
-        rect += ", " + str(self.__height) + ")"
-
-        return (rect)
-
-
+        """Returns the rectangle stringly represented"""
+        new = "Rectangle(" + str(self.__width)
+        new += ", " + str(self.__height) + ")"
+        return new
 
     def __del__(self):
-
-        """Print a message for every deletion of a Rectangle."""
-
+        """Detects a rectangle being deleted"""
         type(self).number_of_instances -= 1
-
         print("Bye rectangle...")
