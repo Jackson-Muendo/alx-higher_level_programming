b62dd55adab8a8abed8c4581b0e6c6bb7596a392
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-06-28 21:36:12 -0700

    corrected

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
-# 0-add_integer.py
-"""Defines an integer addition function."""
+
+'''
+Docstring goes here
+'''
 
 
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
-    Float arguments are typecasted to ints before addition is performed.
-    Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
-    """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    '''add_int'''
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError('b must be an integer')
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
+
+
+if __name__ == '__main__':
+    add_integer = __import__('0-add_integer').add_integer
+
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)
