64e8fe08920bc71dd80d2fd000861e07a8fd9ca7
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-09-22 03:46:57 -0700

    0x10-python-network_0

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response. Also, a header variable X-School-User-Id must be sent with the value 98.
-curl -s "$1" -X GET -H "X-School-User-Id: 98"
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response. A header variable X-School-User-Id must be sent with the value 98
+curl -sX GET -H "X-School-User-Id: 98" "$1"
